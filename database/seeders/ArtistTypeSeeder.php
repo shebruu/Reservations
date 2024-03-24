e340c70fa0ef5dcb644a2c464368c2f1041d7303
ebru <ebsahinn7887@outlook.com>
2024-03-24 10:58:56 +0100

    update artistTypeSeeder to db

--- database/seeders/ArtistTypeSeeder.php
+++ database/seeders/ArtistTypeSeeder.php
@@ -27,16 +27,105 @@ class ArtistTypeSeeder extends Seeder
             [
                 'firstname' => 'Daniel',
                 'lastname' => 'Marcelin',
-                'type' => ['auteur', 'comédien', 'scenographe'],
+                'type' => 'auteur',
             ],
-
             [
-                'firstname' => 'Philipe',
+                'firstname' => 'Philippe',
                 'lastname' => 'Laurent',
-                'type' => ['auteur'],
-
+                'type' => 'auteur',
+            ],
+            [
+                'firstname' => 'Daniel',
+                'lastname' => 'Marcelin',
+                'type' => 'scénographe',
+            ],
+            [
+                'firstname' => 'Philippe',
+                'lastname' => 'Laurent',
+                'type' => 'scénographe',
+            ],
+            [
+                'firstname' => 'Daniel',
+                'lastname' => 'Marcelin',
+                'type' => 'comédien',
+            ],
+            [
+                'firstname' => 'Marius',
+                'lastname' => 'Von Mayenburg',
+                'type' => 'auteur',
+            ],
+            [
+                'firstname' => 'Olivier',
+                'lastname' => 'Boudon',
+                'type' => 'scénographe',
+            ],
+            [
+                'firstname' => 'Anne Marie',
+                'lastname' => 'Loop',
+                'type' => 'comédien',
+            ],
+            [
+                'firstname' => 'Pietro',
+                'lastname' => 'Varasso',
+                'type' => 'comédien',
+            ],
+            [
+                'firstname' => 'Laurent',
+                'lastname' => 'Caron',
+                'type' => 'comédien',
+            ],
+            [
+                'firstname' => 'Élena',
+                'lastname' => 'Perez',
+                'type' => 'comédien',
+            ],
+            [
+                'firstname' => 'Guillaume',
+                'lastname' => 'Alexandre',
+                'type' => 'comédien',
+            ],
+            [
+                'firstname' => 'Claude',
+                'lastname' => 'Semal',
+                'type' => 'auteur',
+            ],
+            [
+                'firstname' => 'Laurence',
+                'lastname' => 'Warin',
+                'type' => 'scénographe',
+            ],
+            [
+                'firstname' => 'Claude',
+                'lastname' => 'Semal',
+                'type' => 'comédien',
+            ],
+            [
+                'firstname' => 'Pierre',
+                'lastname' => 'Wayburn',
+                'type' => 'auteur',
+            ],
+            [
+                'firstname' => 'Gwendoline',
+                'lastname' => 'Gauthier',
+                'type' => 'auteur',
+            ],
+            [
+                'firstname' => 'Philippe',
+                'lastname' => 'Laurent',
+                'type' => 'scénographe',
+            ],
+            [
+                'firstname' => 'Pierre',
+                'lastname' => 'Wayburn',
+                'type' => 'comédien',
+            ],
+            [
+                'firstname' => 'Gwendoline',
+                'lastname' => 'Gauthier',
+                'type' => 'comédien',
             ],
         ];
+
 
 
         foreach ($dataset as &$record) {
--- resources/views/artists/show.blade.php
+++ resources/views/artists/show.blade.php
@@ -3,7 +3,15 @@
 @section('title', ' Fiche d un artiste')
 
 @section('content')
-<h1>{{$artist->firstname}} {{$artist->lastname}}</h1>
+<h1>{{ $artist->firstname }} {{ $artist->lastname }}</h1>
+
+<h2>Liste des types</h2>
+<ul>
+    @foreach($artist->types as $type)
+    <li>{{ $type->type }}</li>
+    @endforeach
+</ul>
+
 
 {{ dump($artist) }}
 
